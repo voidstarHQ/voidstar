5e06e76d6afee9b1d264c7ad18693033ceb626da
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-06 23:24:14 -0800

    master: basic drawing

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-CFLAGS += -Wall -Werror -Wextra -pedantic --std=c++0x
+CFLAGS += -Wall -Wextra -pedantic
+CXXFLAGS += $(CFLAGS) --std=c++0x
 
 all: miners.cc
-	clang++ $(CFLAGS) -o miners -lGL -lGLU -lm -lglut -lglfw -lGLEW miners.cc
+	clang $(CFLAGS) -o miners -lGL -lGLU -lm -lglut -lglfw -lGLEW miners.c
 	./miners
+
+clean:
+	$(if $(wildcard miners), rm miners)
--- miners.cc
+++ miners.cc
@@ -8,6 +8,49 @@
 #include <glm/glm.hpp>
 using namespace glm;
 
+
+// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+};
+
+
 int main( void )
 {
 	// Initialise GLFW
