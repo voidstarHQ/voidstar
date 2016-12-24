6739f0556feaf0809dbd4999d292cb2aaf3e5aaf
Pierre Fenoll <pierrefenoll@gmail.com>
2016-12-24 19:02:06 +0100

    2d: fix 2d view

--- src/include/Algo2D.hh
+++ src/include/Algo2D.hh
@@ -6,7 +6,7 @@ class Algo2D : public Algorithm {
 public:
     virtual ~Algo2D() {}
 
-    virtual SceneType sceneType() const { return SCENE_3D; }
+    virtual SceneType sceneType() const { return SCENE_2D; }
     virtual bool apply(GLfloat* vertices, GLfloat* colors,
                        size_t width, size_t height) = 0;
 };
