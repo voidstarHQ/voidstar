cd84c61bc79c412e38b50c9c6f0b202ca92f8b55
Pierre Fenoll <pierrefenoll@gmail.com>
2016-12-24 19:06:14 +0100

    2d: introduce make_vertices

--- src/include/Algo2D.hh
+++ src/include/Algo2D.hh
@@ -9,4 +9,19 @@ public:
     virtual SceneType sceneType() const { return SCENE_2D; }
     virtual bool apply(GLfloat* vertices, GLfloat* colors,
                        size_t width, size_t height) = 0;
+
+protected:
+    size_t
+    make_vertices(GLfloat* vertices, size_t width, size_t height) {
+        float w = static_cast<float>(width) / 2;
+        float h = static_cast<float>(height) / 2;
+
+        size_t pos = 0;
+        for (size_t y = 0; y < height; ++y)
+            for (size_t x = 0; x < width; ++x) {
+                vertices[pos++] = (static_cast<float>(x) - w) / 128;
+                vertices[pos++] = (static_cast<float>(y) - h) / 128;
+            }
+        return pos / 2;
+    }
 };
--- src/miners/Algo2DGrayscale.cc
+++ src/miners/Algo2DGrayscale.cc
@@ -13,22 +13,17 @@ struct GrayscaleRGBColor {
 bool
 Algo2DGrayscale::apply(GLfloat* vertices, GLfloat* colors,
                        size_t width, size_t height) {
-    size_t pos_v = 0;
-    for (size_t y = 0; y < height; ++y)
-        for (size_t x = 0; x < width; ++x) {
-            vertices[pos_v++] = ((float)x - (float)width  / 2.0f) / 128;
-            vertices[pos_v++] = ((float)y - (float)height / 2.0f) / 128;
-        }
+    make_vertices(vertices, width, height);
 
     const size_t chunk_size = width * height;
     const u8* read = loader_->dataChunk(0, chunk_size);
-    size_t pos_c = 0;
+    size_t pos = 0;
     for (size_t i = 0; i < chunk_size; ++i) {
         auto c = GrayscaleRGBColor(read[i]);
-        colors[pos_c++] = c.r;
-        colors[pos_c++] = c.g;
-        colors[pos_c++] = c.b;
-        colors[pos_c++] = 1.0f;
+        colors[pos++] = c.r;
+        colors[pos++] = c.g;
+        colors[pos++] = c.b;
+        colors[pos++] = 1.0f;
     }
 
     return true;
