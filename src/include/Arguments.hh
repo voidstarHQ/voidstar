a0d981876be40b15bfed715232061c51357fbccd
Pierre Fenoll <pierrefenoll@gmail.com>
2017-04-20 00:19:30 -0700

    nodeco: disable titlebar by default + CLI option (#53)

--- src/include/Arguments.hh
+++ src/include/Arguments.hh
@@ -5,6 +5,17 @@
 #include <memory>
 
 struct Arguments {
+    Arguments() {
+        name = "void*";
+        algo = "conti";
+        manager = "glfw";
+        width = 800;
+        height = 600;
+        range_begin = 0;
+        range_end = 0;
+        fullscreen = false;
+        keep_chrome = false;
+    }
     std::vector<std::string> input;
     std::string name;
     std::string algo;
@@ -14,6 +25,7 @@ struct Arguments {
     size_t range_begin;
     size_t range_end;
     bool fullscreen;
+    bool keep_chrome;
 };
 
 std::shared_ptr<Arguments> parseArgs(int argc, char *argv[]);
