4e5d1441f1bca98a8daaf731e1291fcadc8abb6f
Pierre Fenoll <pierrefenoll@gmail.com>
2020-04-18 18:13:37 +0200

    default-e: do not read whole file by default (#46)
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- voidstar/arguments.cc
+++ voidstar/arguments.cc
@@ -26,7 +26,7 @@ static void usage(const char* prgname) {
     -n, --spin         don't spin shape on itself
 
     -b, --begin        begin offset for the range
-    -e, --end          end offset for the range
+    -e, --end          end offset for the range (0: till end of file)
 
     -h, --help         this help
 
