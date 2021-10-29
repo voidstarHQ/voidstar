61ce37adc25aeb066e0a7c4ba036369b9330b005
Pierre Fenoll <pierrefenoll@gmail.com>
2021-10-29 15:54:18 +0200

    fmt
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- voidstar/size2str.cc
+++ voidstar/size2str.cc
@@ -1,25 +1,24 @@
-#include <cmath>
-
 #include "voidstar/size2str.h"
+
+#include <cmath>
 
 /// 991337 --> "991,337"
 std::string size2str(size_t size) {
   const auto digits = std::ceil(std::log10(size));
-  const auto nonits = static_cast<size_t>(std::ceil(digits / 3) -1);
-  std::string str(static_cast<size_t>(digits)+nonits, '*');
+  const auto nonits = static_cast<size_t>(std::ceil(digits / 3) - 1);
+  std::string str(static_cast<size_t>(digits) + nonits, '*');
 
   int written = 0;
-  for (size_t pos = str.length()-1; pos!=0; pos--) {
+  for (size_t pos = str.length() - 1; pos != 0; pos--) {
     if (written != 0 && written % 3 == 0) {
       str[pos] = '_';
       written = 0;
       continue;
     }
-    str[pos] = static_cast<char>('0' + size%10);
+    str[pos] = static_cast<char>('0' + size % 10);
     size /= 10;
     written++;
   }
-  if (size != 0)
-    str[0] = static_cast<char>('0' + size);
+  if (size != 0) str[0] = static_cast<char>('0' + size);
   return str;
 }
