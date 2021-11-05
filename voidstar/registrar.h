5efce5259869f5118e46656a1e8b9d60e8f3c0d8
Pierre Fenoll <pierrefenoll@gmail.com>
2021-11-05 03:52:39 +0100

    lets try this
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- voidstar/registrar.h
+++ voidstar/registrar.h
@@ -23,10 +23,36 @@
   FOR##FactoryFunc Registered##FOR(const std::string& name);             \
   const std::map<const std::string, FOR##FactoryFunc>& RegistryFor##FOR()
 
-#define REGISTRY_REGISTER_FOR(FOR, NAME, KLASS, FACT)      \
-  void __attribute__((constructor)) Registering##KLASS() { \
-    auto factory = [] FACT;                                \
-    Register##FOR(NAME, factory);                          \
+// https://stackoverflow.com/a/2390626/1418165
+#ifdef __cplusplus
+#define INITIALIZER(f)    \
+  static void f(void);    \
+  struct f##_t_ {         \
+    f##_t_(void) { f(); } \
+  };                      \
+  static f##_t_ f##_;     \
+  static void f(void)
+#elif defined(_MSC_VER)
+#pragma section(".CRT$XCU", read)
+#define INITIALIZER2_(f, p)                                \
+  static void f(void);                                     \
+  __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+  __pragma(comment(linker, "/include:" p #f "_")) static void f(void)
+#ifdef _WIN64
+#define INITIALIZER(f) INITIALIZER2_(f, "")
+#else
+#define INITIALIZER(f) INITIALIZER2_(f, "_")
+#endif
+#else
+#define INITIALIZER(f)                              \
+  static void f(void) __attribute__((constructor)); \
+  static void f(void)
+#endif
+
+#define REGISTRY_REGISTER_FOR(FOR, NAME, KLASS, FACT) \
+  INITIALIZER(Registering##KLASS) {                   \
+    auto factory = [] FACT;                           \
+    Register##FOR(NAME, factory);                     \
   }
 
 /*
